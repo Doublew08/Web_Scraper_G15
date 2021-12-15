ffc56e9a791fa3cb3df9b673f2030c32fe075fbd
Doublew08 <xxxpascadali@gmail.com>
2021-12-15 23:48:25 +0200

    Home

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 with open('First.html','r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find('h1')
-    print(tags)
+    labels_tags = soup.find_all('label')
+    for label in labels_tags:
+        print(label.text)
     
