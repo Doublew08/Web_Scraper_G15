e680b6aad771af70436cdeea66ecb54299ec90a1
Doublew08 <xxxpascadali@gmail.com>
2021-12-15 23:56:18 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 with open('First.html','r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
-    labels_tags = soup.find_all('label')
-    for label in labels_tags:
-        print(label.text)
-    
+    course_cards = soup.find_all('div' ,class_= 'card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(course_name)
+        print(course_price) 
