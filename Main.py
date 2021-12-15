3147f3d8a34c0c18cfb6d823ff534edbb7f26e7c
Doublew08 <xxxpascadali@gmail.com>
2021-12-15 22:23:37 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
-with open('/media/double/DC5A48EB5A48C3CC/Projects/Web-Scraper-/First.html','r') as html_file:
+with open('/media/double/DC5A48EB5A48C3CC/Projects/2/Project/Web_Scraper_G15/First.html','r') as html_file:
     content = html_file.read()
-    print(content)
+    soup = BeautifulSoup(content, 'lxml')
+    print(soup.prettify())
