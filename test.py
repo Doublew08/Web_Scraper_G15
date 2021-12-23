0f3674eecd82ad1e6d2d1b8d726e950c7a874336
Doublew08 <xxxpascadali@gmail.com>
2021-12-24 00:33:23 +0200

    added place of birth and political party

--- test.py
+++ test.py
@@ -15,10 +15,15 @@ politician_name = politician_card.find('div', class_='fn').text
 #print(f'politician name: {politician_name}')
 politician_dob = politician_card.find('span', class_='bday').text
 #print(f'politician date of birth: {politician_dob}')
+politician_bfullname = politician_card.find('div' , class_='nickname').text
+#print(f'politician birth fullname: {politician_bfullname}')
+politician_pob = politician_card.findAll('td', class_='infobox-data')[3].a.text
+#print(f'politician place of birth: {politician_pob}')
+politician_party = politician_card.findAll('td', class_='infobox-data')[4].a.text
+print(f'politician party: {politician_party}')
+Headers = ['politician name','politician date of birth','politician birth fullname']
 
-Headers = ['politician name','politician date of birth']
-
-Categories.extend([politician_name, politician_dob ])
+Categories.extend([politician_name, politician_dob, politician_bfullname ])
 
 Data.append(Categories)
 df =pd.DataFrame(Data, columns = Headers)
