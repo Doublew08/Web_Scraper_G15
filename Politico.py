af5369bf3dccb1b4d454941bedaba6dbdebeec41
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2022-01-06 11:12:51 +0200

    Scraping Politico

---
+++ Politico.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get("https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479")
+soup = BeautifulSoup(response.text,'html.parser')
+for div in soup.find_all('div' , class_ = 'story-text'):
+    for h3 in div.find_all('h3',  class_ = 'story-text__heading-medium label'):
+        print(h3)
