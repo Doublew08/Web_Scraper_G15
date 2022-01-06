e2a9f10a90552026dfc3dcd4c0620bae310b0d13
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2022-01-06 11:38:13 +0200

    Update Politico.py

--- Politico.py
+++ Politico.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 response = requests.get("https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479")
-soup = BeautifulSoup(response.text,'html.parser')
-for div in soup.find_all('div' , class_ = 'story-text'):
-    for h3 in div.find_all('h3',  class_ = 'story-text__heading-medium label'):
-        print(h3)
+soup = BeautifulSoup(response.text, 'html.parser')
+for div in soup.find_all('div', class_='story-text'):
+    for h3s1 in div.find_all('h3',  class_='story-text__heading-medium label') or div.find_all('h3', class_='story-text__heading-medium'):
+        print(h3s1.text)
+
